50844bfaca1b09abcbb46e273dbf84a8b793b416
rayanezerr <rayanelebs@gmail.com>
2023-12-25 18:43:45 -0500

    Card class and Deck class done

---
+++ Card.cs
@@ -0,0 +1,34 @@
+﻿namespace Blackjack;
+
+public class Card
+{
+    public string Type { get; }
+    public string Rank { get; }
+
+    public Card(string type, string rank)
+    {
+        Type = type;
+        Rank = rank;
+    }
+
+    public int GetCardValue()
+    {
+        int cardValue;
+
+        if (int.TryParse(Rank, out cardValue))
+        {
+            return cardValue;
+        }
+        else if (Rank == "Jack" || Rank == "Queen" || Rank == "King")
+        {
+            return 10;
+        }
+        return 0;
+    }
+    
+    //Not sure if i'll use it
+    public override string ToString()
+    {
+        return $"{Rank} of {Type}";
+    }
+}
---
+++ CardDeck.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows.Documents;
+
+namespace Blackjack;
+
+public class CardDeck
+{
+    private List<Card> cardsList;
+
+    public CardDeck()
+    {
+        cardsList = new List<Card>();
+
+        string[] ranks = { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+        string[] types = { "Hearts", "Diamonds", "Spades", "Clubs" };
+
+        foreach (string type in types)
+        {
+            foreach (string rank in ranks)
+            {
+                cardsList.Add(new Card(type, rank));
+            }
+        }
+    }
+
+
+    public void Shuffle()
+    {
+        Random rand = new Random();
+        for (int i = cardsList.Count; i > 1; i--)
+        {
+            int k = rand.Next(i + 1);
+            Card value = cardsList[k];
+            cardsList[k] = cardsList[i];
+            cardsList[i] = value;
+        }
+    }
+}
