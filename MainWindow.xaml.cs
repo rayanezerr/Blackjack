b6a27f20ddc0c415a28b7956cc1d89fa940f753f
rayanezerr <rayanelebs@gmail.com>
2023-12-25 23:04:42 -0500

    Testing

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Collections.Generic;
 
 namespace Blackjack;
 
@@ -19,5 +20,31 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        test();
+    }
+
+    
+    //Testing functions
+    public void test()
+    {
+        CardDeck oeoe = new CardDeck();
+        oeoe.Shuffle();
+        for (int i = 0; i < oeoe.Length(); i++)
+        {
+            Console.WriteLine(oeoe[i]);
+        }
+        Console.WriteLine("=====================");
+        Console.WriteLine(oeoe.Length());
+        for (int i = 0; i < 52; i++)
+        {
+            oeoe.DrawCard();
+        }
+        Console.WriteLine(oeoe.Length());
+        Console.WriteLine("=====================");
+        Console.WriteLine(oeoe.IsDeckEmpty());
+        for (int i = 0; i < oeoe.Length(); i++)
+        {
+            Console.WriteLine(oeoe[i]);
+        }
     }
 }
